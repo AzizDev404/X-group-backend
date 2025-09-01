80659e4898805e9b13001495b1f6c0879bbb0879
Az1z_404 <u03062010@gmail.com>
2025-09-01 14:09:55 -0400

    add enum

--- models/user.model.js
+++ models/user.model.js
@@ -16,6 +16,7 @@ const UserSchema = new mongoose.Schema(
     },
     group: {
       type: String,
+      enum:["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ],
       default: null,
     },
     student_parrent: {
