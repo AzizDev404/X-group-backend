fc0f27db09adfd2b6363a4cb4c4f9d87cc78fdd2
Az1z_404 <u03062010@gmail.com>
2025-09-01 08:01:27 -0400

    update shcema

--- models/user.model.js
+++ models/user.model.js
@@ -9,7 +9,22 @@ const UserSchema = new mongoose.Schema(
     full_name: {
       type: String,
     },
-    nickname: {
+    class: {
+      type: String,
+      enum:["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"],
+      default: null,
+    },
+    group: {
+      type: String,
+      enum:["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+      default: null,
+    },
+    student_parrent: {
+      type: String,
+      enum:["Mother", "Father"],
+      default: null,
+    },
+    parnet_full_name: {
       type: String,
       default: null,
     },
